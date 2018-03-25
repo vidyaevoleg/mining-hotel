ad13114a931fcb5304e476909c0f75a08f3b297e
Oleg Vidyaev <ovidyaev@setka.io>
2018-03-25 22:00:36 +0300

    fix

--- app/services/remote/save_blocks_stat.rb
+++ app/services/remote/save_blocks_stat.rb
@@ -14,10 +14,11 @@ class Remote::SaveBlocksStat
     if machine.block_stats.any?
       pool = machine.template&.url1 || BlockStat.unknown_key
       known_block_stat = machine.block_stats.find_by(pool: pool)
+      byebug
       last_actual_stat = machine.stats.order(id: :asc).where(success: true).last
       if last_actual_stat.blocks_count < stat.blocks_count
         #we found some new block
-        diff = last_actual_stat.blocks_count - stat.blocks_count
+        diff = stat.blocks_count - last_actual_stat.blocks_count
         if known_block_stat
           known_block_stat.update(count: known_block_stat.count + diff)
         else
